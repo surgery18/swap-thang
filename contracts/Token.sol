6c018fdacc83642dd27cc7eba3de3405004dec6f
Scott Deutsch <surgery18@gmail.com>
2021-08-27 22:12:57 -0700

    Start of the project and wrote Token contract and unit tests

---
+++ contracts/Token.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+contract Token {
+    string public name = "Pango Token";
+    string public symbol = "PANGO";
+    uint256 public totalSupply =  1 * (10**9) * (10**18); //1 Billion
+    uint8 public decimals = 18;
+
+    constructor() public {
+        balance[msg.sender] = totalSupply;
+    }
+
+    mapping (address => uint256) balance;
+    mapping (address => mapping(address => uint256)) allowance;
+
+    //events
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    //functions
+    function balanceOf(address _addr) public view returns (uint256) {
+        return balance[_addr];
+    }
+
+    function allowanceOf(address _from, address _addr) external view returns (uint256) {
+        return allowance[_from][_addr];
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool success) {
+        //move your funds to another account
+        require(balance[msg.sender] >= _value, "Not enough tokens");
+        balance[msg.sender] -= _value;
+        balance[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
+        //sender must be allowed to take funds from account A
+        //first of all from address must be allowed to move it to account B
+        require(balance[_from] >= _value, "Not enough tokens");
+        require(allowance[_from][msg.sender] >= _value, "Sender is not allowed to move funds.");
+        //change balances
+        balance[_from] -= _value;
+        balance[_to] += _value;
+        //remove it from the allowance
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) external returns (bool success) {
+        require(balance[msg.sender] >= _value, "Not enough tokens");
+        //attach spender address to sender
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+}
---
+++ test/token.test.js
@@ -0,0 +1,114 @@
+const Token = artifacts.require('Token')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+const toWei = (n) => {
+    return web3.utils.toWei(n, "ether")
+}
+
+const fromWei = (n) => {
+    return web3.utils.fromWei(n, "ether")
+}
+
+const BN = web3.utils.BN;
+
+contract("Token", async ([deployer, accountb, accountc]) => {
+    let token
+    before(async () => {
+        token = await Token.new()
+    })
+
+    describe("Deployment", () => {
+        it('contract has a name', async () => {
+            const name = await token.name()
+            assert.equal(name, 'Pango Token')
+        })
+
+        it('contract has a symbol', async () => {
+            const name = await token.symbol()
+            assert.equal(name, 'PANGO')
+        })
+
+        it('contract has a total supply of 1B tokens', async () => {
+            const supply = await token.totalSupply()
+            assert.equal(supply.toString(), toWei("1000000000"))
+        })
+
+        it("contract supply of tokens should be on the deployer", async () => {
+            const balance = await token.balanceOf(deployer)
+            const supply = await token.totalSupply()
+            assert.equal(balance.toString(), supply.toString())
+        })
+    })
+
+    describe("External Functions", () => {
+        it("Should be able to call the balanceOf function", async () => {
+            const supply = await token.totalSupply()
+            assert.equal(supply.toString(), toWei("1000000000"))
+        })
+
+        it("Should be able to transfer tokens to another account", async () => {
+            //shouldn't be able to transfer more than they own
+            await token.transfer(accountb, toWei("1000000000000")).should.be.rejected 
+
+
+            //should transfer from within the amount they own
+            const result = await token.transfer(accountb, toWei("100"))
+            const event = result.logs[0].args;
+            assert.equal(event._from, deployer)
+            assert.equal(event._to, accountb)
+            assert.equal(event._value.toString(), toWei("100"))
+
+            //account b should have 100 tokens
+            const balance = await token.balanceOf(accountb)
+            assert.equal(balance.toString(), toWei("100"))
+        })
+
+        it("Should be able to call the approve & allowanceOf functions", async () => {
+            //this should fail
+            await token.approve(accountb, toWei("1000000000000")).should.be.rejected
+
+            //this should be good
+            await token.approve(accountb, toWei("1"))
+
+            //now check the account that they can spend the 1 token
+            const allowance = await token.allowanceOf(deployer, accountb)
+            assert.equal(allowance.toString(), toWei("1"))
+        })
+
+        it("Should be able to call the transferFrom function", async () => {
+
+            //transfer some tokens to account c
+            await token.transfer(accountc, toWei("1000"))
+
+            //allow account b to spend on behalf of account c
+            await token.approve(accountb, toWei("500"), {from: accountc})
+
+            //now let account b try to transfer 1000 tokens to deployer account
+            //this should fail since it is only allowed 500 tokens
+            await token.transferFrom(accountc, deployer, toWei("1000"), {from: accountb}).should.be.rejected
+
+            //now try to have deployer take it from account c
+            //this should failed since they are not allowed
+            await token.transferFrom(accountc, deployer, toWei("1")).should.be.rejected
+
+            //get balance of deployer
+            const balanceBefore = await token.balanceOf(deployer)
+
+            //this should work
+            const result = await token.transferFrom(accountc, deployer, toWei("500"), {from: accountb})
+            //check event
+            const event = result.logs[0].args;
+            assert.equal(event._from, accountc)
+            assert.equal(event._to, deployer)
+            assert.equal(event._value.toString(), toWei("500"))
+
+            //check balance of deployer that it has gained 500 tokens
+            const balanceAfter = await token.balanceOf(deployer)
+            let shouldBe = balanceBefore.add(new BN(toWei("500")))
+            assert.equal(balanceAfter.toString(), shouldBe.toString())
+        })
+    })
+})
